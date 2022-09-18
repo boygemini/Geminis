4571c42d0153d84eb6aada4ccc8fd15a1c167be9
Jubril Bolajoko Gbadebo <gbadebojubril@gmail.com>
2022-09-18 04:51:09 +0100

    new commit

--- search.js
+++ search.js
@@ -10,21 +10,27 @@ SEARCH AND SEARCH SUGGESTIONS
 let dir2 = JSON.parse(localStorage.getItem("StoreItems"));
 let hs = document.getElementById("hold");
 let search = document.getElementById("search");
+let go = document.getElementById("GO")
 let dir = dir2.selectedProducts[0];
 let displaybox = document.getElementById("suggestions");
+
+
+
 
 //Function
 const showSuggesttions = (dirname, name) => {
   let x = "";
   let arr = dirname;
+
   let mapp = arr.map((x) => {
     return x.itemInfo.name;
   });
-  // console.log(mapp);
+
   let removeDuplicates = [...new Set(mapp)];
   for (let i in removeDuplicates) {
     let list = removeDuplicates[i].toLowerCase();
     let searchInput = search.value.toLowerCase();
+
     //Show Suggestions
     if (list.includes(searchInput)) {
       x += `<div class="sug"><h1 class="main">${list}</h1><span class="dot"></span><h1 class="in">${name}</h1>
@@ -47,6 +53,20 @@ const showSuggesttions = (dirname, name) => {
   }
 };
 
+
+
+
+const sendQueryGO = (event) => {
+  let query = search.value.toLowerCase();
+  let url = "http://127.0.0.1:5500/HTML/gemshop.html?q=" + encodeURIComponent(query)
+  window.location.href = url;
+};
+try {
+  go.addEventListener("click", sendQueryGO)
+} catch (error) {}
+
+
+
 //DISPLAY SUGGESTIONS
 search.addEventListener("input", () => {
   showSuggesttions(dir.gaming, "Gaming");
@@ -55,6 +75,8 @@ search.addEventListener("input", () => {
   showSuggesttions(dir.computers, "Computers");
   showSuggesttions(dir.tv, "TV");
 });
+
+
 
 
 const sendQuery = (event) => {
@@ -68,31 +90,62 @@ const sendQuery = (event) => {
   window.location.href = url;
 };
 
+
+
+class showResult {
+  static suggestionsResult(newArr, directory, Query, Category) {
+    Category = newArr.category.toLowerCase()
+    directory = dir[`${Category}`]
+    displayResults(directory, Query)
+  }
+
+  static searchBarResult(Query, Category) {
+    Category = ["gaming", "cellphones", "speaker", "computers", "tv"]
+    let arr = []
+    for (let i in Category) {
+      let searchDirectory = dir[`${Category[i]}`];
+      for (let j in searchDirectory) {
+        if (searchDirectory[j].itemInfo.name.toLowerCase().includes(Query)) {
+          arr.push(searchDirectory[j])
+        }
+      }
+    }
+    displayResults(arr, Query)
+  }
+}
+
+
+
 const onLoad = () => {
   let url = document.URL;
   let splitUrl = url.split("?")[1];
   let s2 = splitUrl.split("&");
   let newArr = new Array();
+  let Query, Category, directory;
 
   for (let i in s2) {
     let n = s2[i].split("=");
     newArr[n[0]] = n[1];
   }
 
-  let Query = newArr.q;
-  let Category = newArr.category.toLowerCase()
-  let directory = dir[`${Category}`]
-
-
+  Query = newArr.q
   if (Query.lastIndexOf("%20") > -1) {
     Query = Query.replace(/%20/g, " ");
   }
 
-  displaySuggestion(directory, Query)
+  if (s2.length > 1) {
+    showResult.suggestionsResult(newArr, directory, Query, Category)
+  }
+
+  if (s2.length === 1) {
+    showResult.searchBarResult(Query, Category)
+  }
+
 };
+window.onload = onLoad;
 
 
-const displaySuggestion = (directory, Query) => {
+const displayResults = (directory, Query) => {
   let x = ""
   for (let k in directory) {
     if (directory[k].itemInfo.name.toLowerCase().includes(Query)) {
@@ -119,5 +172,3 @@ const displaySuggestion = (directory, Query) => {
   }
   showBox.innerHTML = `<h1 class="cat-head">Results for "${Query}"</h1>` + x
 }
-
-window.onload = onLoad;
