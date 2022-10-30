5332cfed719cb8ddc5db89ab1083af4eb48aa031
Jubril Bolajoko Gbadebo <gbadebojubril@gmail.com>
2022-10-30 19:23:17 -0400

    new commit

--- filters.js
+++ filters.js
@@ -9,25 +9,51 @@ let brandArray = ["Apple", "Samsung", "Itel", "Infinix", "LG"];
 let ba = []
 let sortedprice = []
 let myBox = []
+let mem = []
+let memB = []
+let items = []
+
+const filterMemory = (pp) => {
+    let n = []
+    for (let i in pp) {
+        for (let k in mem) {
+            if (pp[i].itemInfo.memory === mem[k]) {
+                n.push(pp[i])
+            }
+        }
+    }
+    pp = n;
+    return pp
+}
+
+const disMem = () => {
+    console.log(myBox.length);
+    let memoryBoxes = [...document.querySelectorAll(".memorycheck")]
+    if (myBox.length === 0) {
+        memoryBoxes.filter(box => {
+            box.disabled = true
+        })
+    }
+
+    if (myBox.length > 0) {
+        memoryBoxes.filter(box => {
+            box.disabled = false
+        })
+    }
+
+    if (ba.length > 0) {
+        memoryBoxes.filter(box => {
+            box.disabled = false
+        })
+    }
+}
 
 class filter {
     static price(event, low, high) {
 
         // EVENT TRIGGERED WHEN A BOX IS CHECKED
         if (event.target.checked === true) {
-
             let allItemsInCategory = productRoute.cellphones;
-            let box = [],
-                checkboxes = [...document.querySelectorAll(".pricecheck")];
-
-
-            // Monnitor the number of boxes checked
-            for (let i in checkboxes) {
-                if (checkboxes[i].checked === true) {
-                    box.push(checkboxes[i])
-                }
-            }
-
 
             let clickedBox = {
                 "high": high,
@@ -42,15 +68,26 @@ class filter {
                     return true
                 }
             })
-
-
-
-
-
-
+            disMem()
             // If more than one brand is already selected
             if (ba.length > 0) {
-                let getpriceditems = allItemsInCategory.filter(gp => gp.itemInfo.newItemPrice >= low && gp.itemInfo.newItemPrice <= high);
+                let getpriceditems = allItemsInCategory.filter(gp => Number(gp.itemInfo.newItemPrice) >= low && Number(gp.itemInfo.newItemPrice) <= high);
+                if (pp.length > 0 && myBox.length === 3) {
+                    let n = [];
+                    for (let i in allItemsInCategory) {
+                        for (let k in ba) {
+                            if (allItemsInCategory[i].itemInfo.brand === ba[k]) {
+                                n.push(allItemsInCategory[i])
+                            }
+                        }
+                    }
+                    pp = n;
+                    if (mem.length > 0) {
+                        return filterMemory(pp)
+                    }
+                    return pp
+                }
+
                 for (let i in ba) {
                     for (let k in getpriceditems) {
                         if (ba[i] === getpriceditems[k].itemInfo.brand) {
@@ -59,6 +96,9 @@ class filter {
                     }
                 }
                 newPP = [...new Set(newPP)]
+                if (mem.length > 0) {
+                    return filterMemory(newPP)
+                }
                 return newPP
             }
 
@@ -67,33 +107,42 @@ class filter {
             if (ba.length === 0) {
 
                 // If there are more than one price boxes checked
-                if (box.length > 1) {
+                if (myBox.length > 1) {
                     for (let i in allItemsInCategory) {
                         if (Number(allItemsInCategory[i].itemInfo.newItemPrice) >= low && Number(allItemsInCategory[i].itemInfo.newItemPrice) <= high) {
                             pp.push(allItemsInCategory[i])
                         };
                     }
+                    if (mem.length > 0) {
+                        return filterMemory(pp)
+                    }
                     return pp
                 }
 
                 // If no price boxes checked
-                if (box.length === 1 && pp.length === 0) {
+                if (myBox.length === 1 && pp.length === 0) {
                     pp = []
                     for (let i in allItemsInCategory) {
                         if (Number(allItemsInCategory[i].itemInfo.newItemPrice) >= low && Number(allItemsInCategory[i].itemInfo.newItemPrice) <= high) {
                             pp.push(allItemsInCategory[i])
                         };
                     }
+                    if (mem.length > 0) {
+                        return filterMemory(pp)
+                    }
                     return pp
                 }
 
                 // If no price boxes checked but min-max was set
-                if (box.length === 1 && pp.length > 0) {
+                if (myBox.length === 1 && pp.length > 0) {
                     let pp = []
                     for (let i in allItemsInCategory) {
                         if (Number(allItemsInCategory[i].itemInfo.newItemPrice) >= low && Number(allItemsInCategory[i].itemInfo.newItemPrice) <= high) {
                             pp.push(allItemsInCategory[i])
                         };
+                    }
+                    if (mem.length > 0) {
+                        return filterMemory(pp)
                     }
                     return pp
                 }
@@ -103,24 +152,22 @@ class filter {
         // EVENT TRIGGERED WHEN A BOX IS UN-CHECKED
         if (event.target.checked === false) {
             let allItemsInCategory = productRoute.cellphones;
-            let checkboxes = [...document.querySelectorAll(".pricecheck")]
-            let box = []
-
-
-            for (let i in checkboxes) {
-                if (checkboxes[i].checked === false) {
-                    box.push(checkboxes[i])
-                }
-            }
-
+            myBox = myBox.filter(mb => mb.low !== low && mb.high !== high)
+            console.log(myBox);
+            disMem()
             // If all price boxes are unchecked and no brand boxes is checked
-            if (box.length === 3 && ba.length === 0) {
+            if (myBox.length === 0 && ba.length === 0) {
                 pp = productRoute.cellphones;
-                return pp;
+                if (mem.length > 0) {
+                    return filterMemory(pp)
+                }
+                return pp
             }
+
+
 
             // If all price boxes are unchecked while brand boxes are jus being checked
-            if (box.length === 3 && ba.length > 0) {
+            if (myBox.length === 3 && ba.length > 0) {
                 let getthemfirst = []
                 for (let a in allItemsInCategory) {
                     for (let c in ba) {
@@ -131,11 +178,14 @@ class filter {
                 }
                 getthemfirst = [...new Set(getthemfirst)]
                 pp = getthemfirst
+                if (mem.length > 0) {
+                    return filterMemory(pp)
+                }
                 return pp
             }
 
             // HERE NOW......
-            if (box.length < 3 && ba.length > 0) {
+            if (myBox.length < 3 && ba.length > 0) {
                 let restFilter = pp.filter(rest => Number(rest.itemInfo.newItemPrice) >= low && Number(rest.itemInfo.newItemPrice) <= high)
                 pp = pp.filter(item => !restFilter.includes(item))
                 let tp = []
@@ -146,16 +196,20 @@ class filter {
                         }
                     }
                 }
-                console.log(pp);
                 pp = tp
+                if (mem.length > 0) {
+                    return filterMemory(pp)
+                }
                 return pp
             }
 
 
-            if (box.length < 3 && ba.length === 0) {
+            if (myBox.length > 0 && ba.length === 0) {
                 let restFilter = pp.filter(rest => Number(rest.itemInfo.newItemPrice) >= low && Number(rest.itemInfo.newItemPrice) <= high)
                 pp = pp.filter(item => !restFilter.includes(item))
-                console.log(pp)
+                if (mem.length > 0) {
+                    return filterMemory(pp)
+                }
                 return pp
             }
 
@@ -200,7 +254,7 @@ class filter {
         }
 
         if (event.target.checked === true) {
-
+            disMem()
             // Monitor the number of brand boxes selected
             for (let i in brandArray) {
                 if (brandArray[i] === brand) {
@@ -216,6 +270,9 @@ class filter {
                         pp.push(allItemsInCategory[i])
                     }
                 }
+                if (mem.length > 0) {
+                    return filterMemory(pp)
+                }
                 return pp
             }
 
@@ -227,6 +284,9 @@ class filter {
                     }
                 }
                 pp = [...new Set(pp)]
+                if (mem.length > 0) {
+                    return filterMemory(pp)
+                }
                 return pp
             }
 
@@ -238,7 +298,10 @@ class filter {
                             newPP.push(pp[i])
                         }
                     }
-                    return newPP;
+                    if (mem.length > 0) {
+                        return filterMemory(newPP)
+                    }
+                    return newPP
                 }
 
                 if (newPP.length > 0) {
@@ -248,7 +311,10 @@ class filter {
                         }
                     }
                     newPP = [...new Set(newPP)]
-                    return newPP;
+                    if (mem.length > 0) {
+                        return filterMemory(newPP)
+                    }
+                    return newPP
                 }
             }
 
@@ -268,63 +334,151 @@ class filter {
                     }
                 }
                 newPP = gp
+                if (mem.length > 0) {
+                    return filterMemory(newPP)
+                }
                 return newPP
             }
-
-            // if (pp.length > 0 && newPP.length === 0) {
-            //     for (let i in pp) {
-            //         if (pp[i].itemInfo.brand === brand) {
-            //             newPP.push(pp[i])
-            //         }
-            //     }
-            //     return newPP
-            // }
-
-            // if (newPP.length > 0) {
-            //     let mp = newPP.filter(item => item.itemInfo.brand === brand);
-
-            //     if (mp.length === 0) {
-            //         console.log("There are no such branches");
-            //         for (let j in pp) {
-            //             if (pp[j].itemInfo.brand === brand) {
-            //                 newPP.push(pp[j]);
-            //             }
-            //         }
-            //         return newPP;
-            //     }
-
-            //     if (mp.length > 0) {
-            //         return mp
-            //     }
-            // }
         }
 
-        // if (event.target.checked === false) {
-        //     let box = []
+        if (event.target.checked === false) {
+            let uncheckedBox = ba.filter(box => box !== brand)
+            let checkboxes = [...document.querySelectorAll(".brandcheck")]
 
-        //     let checkboxes = [...document.querySelectorAll(".brandcheck")]
-        //     for (let i in checkboxes) {
-        //         if (checkboxes[i].checked === false) {
-        //             box.push(checkboxes[i])
-        //         }
-        //     }
 
-        //     ba = ba.filter(b => b !== brand)
+            if (uncheckedBox.length === 0 && myBox.length > 0 && myBox.length < 3) {
+                let itemsInPrice = allItemsInCategory.filter(item => Number(item.itemInfo.newItemPrice) >= myBox[0].low && Number(item.itemInfo.newItemPrice) <= myBox[myBox.length - 1].high);
+                pp = itemsInPrice;
+                if (mem.length > 0) {
+                    return filterMemory(pp)
+                }
+                return pp
+            }
 
-        //     if (pp.length > 0 && box.length < checkboxes.length) {
-        //         pp = pp.filter(p => p.itemInfo.brand !== brand)
-        //         return pp
-        //     }
 
-        //     if (box.length === checkboxes.length) {
-        //         return pp
-        //     }
-        // }
+            // If more than one price boxes is checked and brand box is unchecked afresh
+            if (myBox.length > 0 && myBox.length < 3 && ba.length > 0) {
+                let itemsInPrice = allItemsInCategory.filter(item => Number(item.itemInfo.newItemPrice) >= myBox[0].low && Number(item.itemInfo.newItemPrice) <= myBox[myBox.length - 1].high);
+                ba = ba.filter(b => b !== brand)
+                let n = []
+                for (let i in itemsInPrice) {
+                    for (let k in ba) {
+                        if (itemsInPrice[i].itemInfo.brand === ba[k]) {
+                            n.push(itemsInPrice[i])
+                        }
+                    }
+                }
+                pp = n;
+
+                if (mem.length > 0) {
+                    return filterMemory(pp)
+                }
+                return pp
+            }
+
+
+
+            if (myBox.length === 3) {
+                let n = []
+                ba = ba.filter(b => b !== brand)
+                for (let i in allItemsInCategory) {
+                    for (let k in ba) {
+                        if (allItemsInCategory[i].itemInfo.brand === ba[k]) {
+                            n.push(allItemsInCategory[i])
+                        }
+                    }
+                }
+                pp = n;
+                if (mem.length > 0) {
+                    return filterMemory(pp)
+                }
+                return pp
+            }
+
+            // If a box is already checked but rest are unchecked
+            if (pp.length > 0 && uncheckedBox.length < checkboxes.length) {
+                pp = pp.filter(p => p.itemInfo.brand !== brand)
+                if (mem.length > 0) {
+                    return filterMemory(pp)
+                }
+                return pp
+            }
+
+            // If all boxes are unchecked
+            if (box.length === checkboxes.length) {
+                if (mem.length > 0) {
+                    return filterMemory(pp)
+                }
+                return pp
+            }
+        }
     }
 
 
-    static memory() {
+    static memory(event, space) {
+        let allItemsInCategory = productRoute.cellphones;
+        if (event.target.checked === true) {
+            mem.push(space)
+            let newSet = new Set()
+            mem = mem.filter(mm => {
+                if (!newSet.has(mm)) {
+                    newSet.add(mm)
+                    return true
+                }
+            })
 
+
+
+            if (ba.length > 0) {
+                let n = []
+                let t = []
+                for (let i in allItemsInCategory) {
+                    for (let k in ba) {
+                        if (allItemsInCategory[i].itemInfo.brand === ba[k]) {
+                            n.push(allItemsInCategory[i])
+                        }
+                    }
+                }
+                for (let k in n) {
+                    for (let y in mem) {
+                        if (n[k].itemInfo.memory === mem[y]) {
+                            t.push(n[k])
+                        }
+                    }
+                }
+                pp = t;
+                return pp
+            }
+
+
+            if (myBox.length > 0) {
+                let getpriceditems = allItemsInCategory.filter(gp => Number(gp.itemInfo.newItemPrice) >= myBox[0].low && Number(gp.itemInfo.newItemPrice) <= myBox[myBox.length - 1].high);
+                let n = [];
+                for (let i in getpriceditems) {
+                    for (let k in mem) {
+                        if (getpriceditems[i].itemInfo.memory === mem[k]) {
+                            n.push(getpriceditems[i])
+                        }
+                    }
+                }
+                pp = n;
+                return pp
+            }
+        }
+
+        if (event.target.checked === false) {
+            mem = mem.filter(box => box !== space)
+            let n = []
+            for (let i in pp) {
+                for (let j in mem) {
+                    if (pp[i].itemInfo.memory === mem[j]) {
+                        n.push(pp[i])
+                    }
+                }
+            }
+            pp = n
+            return pp
+        }
     }
 
 
@@ -361,7 +515,7 @@ const displayFilteredResults = (directory) => {
             <img src=${directory[k].itemInfo.itemImg} alt="">
             <div class="item-details">
                 <h1>${directory[k].itemInfo.name}</h1>
-                <h2>${directory[k].itemInfo.description1}</h2>
+                <h2>${directory[k].itemInfo.description1} ${directory[k].itemInfo.memory}GB</h2>
                 <div class="specifications">
                     <strong>Refurbished</strong>
                     <p><strong>Model : </strong>MKLV3LL/A</p>
