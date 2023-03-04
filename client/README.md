a4094186592fb4ca115d09c1557af25134480d28
Chief Cook [JB] <user@MacBook-Pro.local>
2023-03-04 05:48:26 -0600

    deploy

---
+++ client/README.md
@@ -0,0 +1,2 @@
+# Geministore
+E-commerce website
---
+++ client/m.js
@@ -0,0 +1,12 @@
+let i = 1;
+let b = 2;
+if (i < b) {
+	console.log("Yes, i is less than b");
+} else {
+	console.log("i is not less than b");
+}
+
+// const check = (i, b) => {
+// 	i < b ? console.log("Yes, i is less than b") : "i is not less thab b";
+// };
+// check(1, 2);
