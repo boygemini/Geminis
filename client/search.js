caf7c93129758ab868555ec6a5655e39c63dc10f
Chief Cook [JB] <user@MacBook-Pro.local>
2023-03-04 11:02:52 -0600

    few changed

--- client/search.js
+++ client/search.js
@@ -233,24 +233,21 @@ class getResults {
 	}
 
 	static async negativeResults(Query) {
-		let products = await allProducts();
-		if (products) {
-			showBox.innerHTML = `<div class="noresult">
+		showBox.innerHTML = `<div class="noresult">
 			<div class ="noresulttext"><h1 class = "cat-head" id="nores-h1"> Oops, there are no results for  "${Query}" </h1><p>Try checking your spelling or use more general terms</p></div>
 			<span>Go back to</span>
 			<div class="noresultbuttons"><a href="index.html" class='backtohome'>Homepage</a><a href="gemshop.html" class='backtoshop'>Shop</a></div>
-		</div>`;
-			document.getElementById("filter").style.display = "none";
-			document.querySelector(".sort-hol").style.display = "none";
-			document.querySelector(".res-sort").style.display = "none";
-			document.querySelector(".pgd").style.display = "none";
-			document.querySelector(".littlebans").style.display = "none";
-			document.querySelector(".banner-container").style.display = "none";
-			document.getElementById("bodyy").style.backgroundColor = "#f4f4f6";
-			document.getElementById("footer").style.position = "absolute";
-			document.getElementById("footer").style.bottom = "0";
-			showBox.classList.remove("showboxgrid");
-		}
+			</div>`;
+		document.getElementById("filter").style.display = "none";
+		document.querySelector(".sort-hol").style.display = "none";
+		document.querySelector(".res-sort").style.display = "none";
+		document.querySelector(".pgd").style.display = "none";
+		document.querySelector(".littlebans").style.display = "none";
+		document.querySelector(".banner-container").style.display = "none";
+		document.getElementById("bodyy").style.backgroundColor = "#f4f4f6";
+		document.getElementById("footer").style.position = "absolute";
+		document.getElementById("footer").style.bottom = "0";
+		showBox.classList.remove("showboxgrid");
 	}
 
 	static async positiveResults() {
@@ -403,15 +400,9 @@ const getCatFiltersAndSearchResults = async (Query, Category) => {
 	let general = [gamekey, phonekey, tvkey, speakerkey, comkey];
 	let generalKeyWords = new Array();
 
-	for (let i in general) {
-		for (let j in general[i]) {
-			generalKeyWords.push(general[i][j]);
-		}
-	}
-
 	for (let i in generalKeyWords) {
 		if (generalKeyWords[i].toLowerCase() !== Query) {
-			getResults.negativeResults(Query);
+			return getResults.negativeResults(Query);
 		}
 	}
 
@@ -456,6 +447,12 @@ const getCatFiltersAndSearchResults = async (Query, Category) => {
 		if (comkey[i].toLowerCase().includes(Query)) {
 			Computers();
 			Call(Query, Category);
+		}
+	}
+
+	for (let i in general) {
+		for (let j in general[i]) {
+			generalKeyWords.push(general[i][j]);
 		}
 	}
 };
